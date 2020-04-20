776c896a2dc5db6cb664a56de8150111fc0ba054
LeoBurette <54930754+LeoBurette@users.noreply.github.com>
2020-04-20 08:30:55 +0200

    admin

--- front/src/services/admin.service.ts
+++ front/src/services/admin.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { serverUrl } from '../configs/server.config';
+import {httpOptionsBase, serverUrl} from '../configs/server.config';
 import {Admin} from '../models/admin.model';
 import {BehaviorSubject} from 'rxjs';
+import {Patient} from "../models/patient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,24 @@ export class AdminService {
   private admins: Admin[];
   public admins$: BehaviorSubject<Admin[]> = new BehaviorSubject(this.admins);
   private adminUrl = serverUrl + '/admins';
+  private httpOptions = httpOptionsBase;
 
   constructor(private http: HttpClient) {
 
   }
 
   getAdmins() {
+    this.http.get<Admin[]>(this.adminUrl).subscribe((adminList) => {
+      this.admins = adminList;
+      this.admins$.next(this.admins);
+    });
+  }
+
+  addAdmin(admin: Admin) {
+    this.http.post<Admin>(this.adminUrl, admin, this.httpOptions).subscribe(() => this.setAdminFromUrl());
+  }
+
+  setAdminFromUrl() {
     this.http.get<Admin[]>(this.adminUrl).subscribe((adminList) => {
       this.admins = adminList;
       this.admins$.next(this.admins);
