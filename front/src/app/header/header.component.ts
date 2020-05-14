35b3566a6886248076f8d7a48875b612dc3108dd
LeoBurette <54930754+LeoBurette@users.noreply.github.com>
2020-05-14 19:36:03 +0200

    premier essaie header

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +9,19 @@ export class HeaderComponent implements OnInit {
 
   public user;
 
+  @Input() admin: boolean;
+
   constructor() {
     this.user = localStorage.getItem('user');
   }
 
+
   ngOnInit() {
+    this.printSpace();
+  }
+
+
+  printSpace() {
+    document.getElementById('space').innerText = 'Espace '.concat((this.admin) ? 'encadrant' : 'patient');
   }
 }
