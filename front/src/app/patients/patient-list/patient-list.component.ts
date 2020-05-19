77e79b3d67e4c0f38ba088541a198cd904cb4333
JeromeFroment <43061265+JeromeFroment@users.noreply.github.com>
2020-05-19 16:27:36 +0200

    mock

--- front/src/app/patients/patient-list/patient-list.component.ts
+++ front/src/app/patients/patient-list/patient-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
 import {Patient} from '../../../models/patient.model';
 import {PatientService} from '../../../services/patient.service';
